f3eadc5055ae57038d42a35012cda5857a83f293
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-10 19:38:53 +0100

    wip

--- users/users-router.js
+++ users/users-router.js
@@ -3,6 +3,10 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
 
+function roleChecker(role) {
+  return 
+}
+
 router.get("/", restricted, (req, res) => {
   Users.find()
     .then(users => {
